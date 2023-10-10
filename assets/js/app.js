1c97a4604a5500d8fc04e5e7542fd721fae1665f
unknown <malanabouble11@gmail.com>
2023-10-10 10:25:08 +0700

    upload pertama

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// event saat link di klik
+//   $(".page-scroll").on("click", function(e){
+//    // ambil isi href
+//    var tujuan = $(this).attr("href");
+   
+//    // ambil element yang bersangkutan
+//    var elemenTujuan = $(tujuan);
+
+//    // pindahkan Scroll
+//    $("body").animate({
+//       scrollTop: elemenTujuan.offset().top - 200
+//    }, 1500,"easeOutBounce");
+
+//    // e.preventDefault();
+   
+// });
