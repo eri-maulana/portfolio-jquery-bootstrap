168d68abfc76ea0e43231d27c45c4bf687728bb7
eri-maulana <maulanabouble11@gmail.com>
2023-10-11 10:33:38 +0700

    menambah efek parallax

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,35 @@
 //    // e.preventDefault();
    
 // });
+
+// About
+$(window).on('load', function(){
+   $('.pKiri').addClass('pMuncul');
+   $('.pKanan').addClass('pMuncul');
+});
+
+// parallax
+$(window).scroll(function () {
+   var wScroll = $(this).scrollTop();
+
+   // jumbotron
+   $('.jumbotron img').css({
+      'transform': 'translate(0px, ' + wScroll/4 + '%)',
+   })
+   $('.jumbotron h1').css({
+      'transform': 'translate(0px, ' + wScroll/2 + '%)',
+   })
+   $('.jumbotron p').css({
+      'transform': 'translate(0px, ' + wScroll/1.2 + '%)',
+   })
+
+   // portfolio
+   if ( wScroll > $('.portfolio').offset().top - 200) {
+      $('.portfolio .img-thumbnail').each(function (i) {
+         setTimeout(function () {
+            $('.portfolio .img-thumbnail').eq(i).addClass('muncul');
+         }, 500 * i);
+      });
+   }
+
+});
